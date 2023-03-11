28da101035dc3fb04e2924a84b16e1e0cee20737
Jammel Johnson <jammel.johnson@myguardiangroup.com>
2023-03-11 10:54:24 -0500

    tweak header and update image ref

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import NavLinks from "./NavLinks";
-import logo from "../images/logo.svg";
+import logo from "../images/logo.png";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="header">
-      <img className="logo" src={logo} alt="logo" />
+      <NavLink to="/">
+        <img className="logo" src={logo} alt="logo" />
+      </NavLink>
       <NavLinks />
     </header>
   );
